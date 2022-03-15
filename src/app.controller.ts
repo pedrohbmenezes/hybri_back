755ee3f3735aa9bcd355bc3e417a9714696ba160
Pedro Henrique <62969781+pedrohbrandao@users.noreply.github.com>
2022-03-15 03:17:25 -0300

    auth

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { AuthService } from './auth/auth.service';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService, private authService: AuthService) {}
+
+  @Get()
+    @UseGuards(AuthGuard('jwt'))
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+}
